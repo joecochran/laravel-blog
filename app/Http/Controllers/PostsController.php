b833941a4b185ac1dac6d59e577a80bd3af4ba98
Joe Cochran <j_cochran@me.com>
2015-12-27 13:03:00 -0800

    docblocking Post controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,24 +9,47 @@ use Carbon\Carbon;
 
 class PostsController extends Controller
 {
+    /**
+     * Return all posts
+     *
+     * @return Response
+     */
     public function index()
     {
         $posts = Post::latest('published_at')->published()->get();
 
         return view('posts.index', compact('posts'));
     }
-
+    
+    /**
+     * Show specific post
+     *
+     * @param integer $id
+     * @return Response
+     */
     public function show($id)
     {
         $post = Post::findOrFail($id);
 
         return view('posts.show', compact('post'));
     }
-
+    
+    /**
+     * Show form to create new article
+     *
+     * @return Response
+     */
     public function create()
     {
         return view('posts.create');
     }
+
+    /**
+     * Store a new article
+     *
+     * @param CreatePostRequest $request
+     * @return Response
+     */
     public function store(Requests\CreatePostRequest $request)
     {
         Post::create($request->all());
